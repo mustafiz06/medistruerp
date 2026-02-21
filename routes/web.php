b222f42e4e8f1539ccf9372a5895da1919fdd13b
mustafiz06 <themustafiz06@gmail.com>
2026-02-21 14:14:54 +0600

    route organized by make group

--- routes/web.php
+++ routes/web.php
@@ -28,83 +28,80 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
+//product setting route
+Route::prefix('productsetting')->group(function () {
+    Route::get('/category', [CategoryController::class, 'index'])->name('category.index');
+    Route::post('/category/store', [CategoryController::class, 'store'])->name('category.store');;
+    Route::post('/category/delete/{id}/', [CategoryController::class, 'delete'])->name('category.delete');
+    Route::post('/category/status/{id}/', [CategoryController::class, 'status'])->name('category.status');
+    Route::post('/category/update/{id}/', [CategoryController::class, 'update'])->name('category.update');
 
-// Category  Route
-Route::get('/productsetting/category', [CategoryController::class, 'index'])->name('category.index');
-Route::post('/productsetting/category/store', [CategoryController::class, 'store'])->name('category.store');;
-Route::post('/productsetting/category/delete/{id}/', [CategoryController::class, 'delete'])->name('category.delete');
-Route::post('/productsetting/category/status/{id}/', [CategoryController::class, 'status'])->name('category.status');
-Route::post('/productsetting/category/update/{id}/', [CategoryController::class, 'update'])->name('category.update');
+    Route::get('/unit', [UnitController::class, 'index'])->name('unit.index');
+    Route::post('/unit/store', [UnitController::class, 'store'])->name('unit.store');;
+    Route::post('/unit/delete/{id}/', [UnitController::class, 'delete'])->name('unit.delete');
+    Route::post('/unit/status/{id}/', [UnitController::class, 'status'])->name('unit.status');
+    Route::post('/unit/edit/{id}/', [UnitController::class, 'edit'])->name('unit.edit');
 
+    Route::get('/brand', [BrandController::class, 'index'])->name('brand.index');
+    Route::post('/brand/store', [BrandController::class, 'store'])->name('brand.store');;
+    Route::post('/brand/delete/{id}/', [BrandController::class, 'delete'])->name('brand.delete');
+    Route::post('/brand/status/{id}/', [BrandController::class, 'status'])->name('brand.status');
+    Route::post('/brand/edit/{id}/', [BrandController::class, 'edit'])->name('brand.edit');
 
-Route::get('/productsetting/unit', [UnitController::class, 'index'])->name('unit.index');
-Route::post('/productsetting/unit/store', [UnitController::class, 'store'])->name('unit.store');;
-Route::post('/productsetting/unit/delete/{id}/', [UnitController::class, 'delete'])->name('unit.delete');
-Route::post('/productsetting/unit/status/{id}/', [UnitController::class, 'status'])->name('unit.status');
-Route::post('/productsetting/unit/edit/{id}/', [UnitController::class, 'edit'])->name('unit.edit');
+    Route::get('/country', [CountryController::class, 'index'])->name('country.index');
+    Route::post('/country/store', [CountryController::class, 'store'])->name('country.store');;
+    Route::post('/country/delete/{id}/', [CountryController::class, 'delete'])->name('country.delete');
+    Route::post('/country/status/{id}/', [CountryController::class, 'status'])->name('country.status');
+    Route::post('/country/edit/{id}/', [CountryController::class, 'edit'])->name('country.edit');
 
+    Route::get('/product', [ProductController::class, 'index'])->name('product.index');
+    Route::get('/product/add', [ProductController::class, 'add'])->name('product.add');
+    Route::post('/product/store', [ProductController::class, 'store'])->name('product.store');;
+    Route::post('/product/delete/{id}/', [ProductController::class, 'delete'])->name('product.delete');
+    Route::post('/product/status/{id}/', [ProductController::class, 'status'])->name('product.status');
+    Route::get('/product/edit/{id}', [ProductController::class, 'edit_view'])->name('product.edit.view');
+    Route::post('/product/edit/{id}/', [ProductController::class, 'update'])->name('product.update');
+    Route::get('/barcode/generate/{product}', [BarcodeController::class, 'generate'])->name('product.barcode.generate');
+});
 
-Route::get('/productsetting/brand', [BrandController::class, 'index'])->name('brand.index');
-Route::post('/productsetting/brand/store', [BrandController::class, 'store'])->name('brand.store');;
-Route::post('/productsetting/brand/delete/{id}/', [BrandController::class, 'delete'])->name('brand.delete');
-Route::post('/productsetting/brand/status/{id}/', [BrandController::class, 'status'])->name('brand.status');
-Route::post('/productsetting/brand/edit/{id}/', [BrandController::class, 'edit'])->name('brand.edit');
+//account setting route
+Route::prefix('accountsetting')->group(function () {
+    Route::get('/paymentmethod', [PaymentMethodController::class, 'index'])->name('paymentMethod.index');
+    Route::post('/paymentmethod/store', [PaymentMethodController::class, 'store'])->name('paymentMethod.store');;
+    Route::post('/paymentmethod/delete/{id}/', [PaymentMethodController::class, 'delete'])->name('paymentMethod.delete');
+    Route::post('/paymentmethod/status/{id}/', [PaymentMethodController::class, 'status'])->name('paymentMethod.status');
+    Route::post('/paymentmethod/edit/{id}/', [PaymentMethodController::class, 'edit'])->name('paymentMethod.edit');
 
+    Route::get('/expensehead', [ExpenseHeadController::class, 'index'])->name('expenseHead.index');
+    Route::post('/expensehead/store', [ExpenseHeadController::class, 'store'])->name('expenseHead.store');;
+    Route::post('/expensehead/delete/{id}/', [ExpenseHeadController::class, 'delete'])->name('expenseHead.delete');
+    Route::post('/expensehead/status/{id}/', [ExpenseHeadController::class, 'status'])->name('expenseHead.status');
+    Route::post('/expensehead/edit/{id}/', [ExpenseHeadController::class, 'edit'])->name('expenseHead.edit');
+});
 
-Route::get('/productsetting/country', [CountryController::class, 'index'])->name('country.index');
-Route::post('/productsetting/country/store', [CountryController::class, 'store'])->name('country.store');;
-Route::post('/productsetting/country/delete/{id}/', [CountryController::class, 'delete'])->name('country.delete');
-Route::post('/productsetting/country/status/{id}/', [CountryController::class, 'status'])->name('country.status');
-Route::post('/productsetting/country/edit/{id}/', [CountryController::class, 'edit'])->name('country.edit');
+//customer route
+Route::prefix('customer')->group(function () {
+    Route::get('/', [CustomerController::class, 'index'])->name('customer.index');
+    Route::get('/add', [CustomerController::class, 'add'])->name('customer.add');
+    Route::post('/store', [CustomerController::class, 'store'])->name('customer.store');;
+    Route::post('/delete/{id}/', [CustomerController::class, 'delete'])->name('customer.delete');
+    Route::post('/status/{id}/', [CustomerController::class, 'status'])->name('customer.status');
+    Route::get('/edit/{id}', [CustomerController::class, 'edit_view'])->name('customer.edit.view');
+    Route::post('/edit/{id}/', [CustomerController::class, 'update'])->name('customer.update');
+});
 
+//supplier route
+Route::prefix('customer')->group(function () {
+    Route::get('/', [SupplierController::class, 'index'])->name('supplier.index');
+    Route::get('/add', [SupplierController::class, 'add'])->name('supplier.add');
+    Route::post('/store', [SupplierController::class, 'store'])->name('supplier.store');;
+    Route::post('/delete/{id}/', [SupplierController::class, 'delete'])->name('supplier.delete');
+    Route::post('/status/{id}/', [SupplierController::class, 'status'])->name('supplier.status');
+    Route::get('/edit/{id}', [SupplierController::class, 'edit_view'])->name('supplier.edit.view');
+    Route::post('/edit/{id}/', [SupplierController::class, 'update'])->name('supplier.update');
+});
 
-Route::get('/accountsetting/paymentmethod', [PaymentMethodController::class, 'index'])->name('paymentMethod.index');
-Route::post('/accountsetting/paymentmethod/store', [PaymentMethodController::class, 'store'])->name('paymentMethod.store');;
-Route::post('/accountsetting/paymentmethod/delete/{id}/', [PaymentMethodController::class, 'delete'])->name('paymentMethod.delete');
-Route::post('/accountsetting/paymentmethod/status/{id}/', [PaymentMethodController::class, 'status'])->name('paymentMethod.status');
-Route::post('/accountsetting/paymentmethod/edit/{id}/', [PaymentMethodController::class, 'edit'])->name('paymentMethod.edit');
-
-
-
-Route::get('/accountsetting/expensehead', [ExpenseHeadController::class, 'index'])->name('expenseHead.index');
-Route::post('/accountsetting/expensehead/store', [ExpenseHeadController::class, 'store'])->name('expenseHead.store');;
-Route::post('/accountsetting/expensehead/delete/{id}/', [ExpenseHeadController::class, 'delete'])->name('expenseHead.delete');
-Route::post('/accountsetting/expensehead/status/{id}/', [ExpenseHeadController::class, 'status'])->name('expenseHead.status');
-Route::post('/accountsetting/expensehead/edit/{id}/', [ExpenseHeadController::class, 'edit'])->name('expenseHead.edit');
-
-
-Route::get('/customer', [CustomerController::class, 'index'])->name('customer.index');
-Route::get('/customer/add', [CustomerController::class, 'add'])->name('customer.add');
-Route::post('/customer/store', [CustomerController::class, 'store'])->name('customer.store');;
-Route::post('/customer/delete/{id}/', [CustomerController::class, 'delete'])->name('customer.delete');
-Route::post('/customer/status/{id}/', [CustomerController::class, 'status'])->name('customer.status');
-Route::get('/customer/edit/{id}', [CustomerController::class, 'edit_view'])->name('customer.edit.view');
-Route::post('/customer/edit/{id}/', [CustomerController::class, 'update'])->name('customer.update');
-
-
-Route::get('/supplier', [SupplierController::class, 'index'])->name('supplier.index');
-Route::get('/supplier/add', [SupplierController::class, 'add'])->name('supplier.add');
-Route::post('/supplier/store', [SupplierController::class, 'store'])->name('supplier.store');;
-Route::post('/supplier/delete/{id}/', [SupplierController::class, 'delete'])->name('supplier.delete');
-Route::post('/supplier/status/{id}/', [SupplierController::class, 'status'])->name('supplier.status');
-Route::get('/supplier/edit/{id}', [SupplierController::class, 'edit_view'])->name('supplier.edit.view');
-Route::post('/supplier/edit/{id}/', [SupplierController::class, 'update'])->name('supplier.update');
-
-
-
-Route::get('/productsetting/product', [ProductController::class, 'index'])->name('product.index');
-Route::get('/productsetting/product/add', [ProductController::class, 'add'])->name('product.add');
-Route::post('/productsetting/product/store', [ProductController::class, 'store'])->name('product.store');;
-Route::post('/productsetting/product/delete/{id}/', [ProductController::class, 'delete'])->name('product.delete');
-Route::post('/productsetting/product/status/{id}/', [ProductController::class, 'status'])->name('product.status');
-Route::get('/productsetting/product/edit/{id}', [ProductController::class, 'edit_view'])->name('product.edit.view');
-Route::post('/productsetting/product/edit/{id}/', [ProductController::class, 'update'])->name('product.update');
-
-
-
-Route::get('/productsetting/barcode/generate/{product}', [BarcodeController::class, 'generate'])->name('product.barcode.generate');
-
-
+//stock route
 Route::prefix('stock')->group(function () {
     Route::get('/opening', [StockController::class, 'openingStockForm'])->name('stock.openingForm');
     Route::post('/opening/save', [StockController::class, 'openingStockSave'])->name('stock.openingSave');
